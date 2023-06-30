3b1669e46dae86ba699ee7b00b56e042f5ad4a2f
Ravil <hi-rav@yandex.com>
2023-06-30 23:50:14 +0300

    Added Task 40 + Task 42 + small additions to Task 36

--- HW_7/Task_36.py
+++ HW_7/Task_36.py
@@ -10,13 +10,23 @@
 #                                             3 6 9 12 15 18
 #                                             4 8 12 16 20 24
 #                                             5 10 15 20 25 30
-#                                             6 12 18 24 30 36 
+#                                             6 12 18 24 30 36
 
-def print_operation_table(operation, num_rows = 6, num_columns = 6):
-    for i in range(1, num_rows + 1):
-        table = []
-        for j in range(1, num_columns + 1):
-            table.append(str(operation(i,j)))          
-        print('\t'.join(table))                        
+# def print_operation_table(operation, num_rows = 6, num_columns = 6):
+#     for i in range(1, num_rows + 1):
+#         table = []
+#         for j in range(1, num_columns + 1):
+#             table.append(str(operation(i,j)))
+#         print('\t'.join(table))
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    table_list = [
+        [operation(i, j) for i in range(1, num_rows + 1)]
+        for j in range(1, num_columns + 1)
+    ]
+    for line in table_list:
+        print(*line)
+
 
 print_operation_table(lambda x, y: x * y)
